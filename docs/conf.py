465581c00ca6ad48a5edca8e034e05a94a17adce
Tim Anhalt <tim@anhalt.me>
2022-03-06 15:40:00 +0100

    Set doc theme to RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Tim Anhalt (BitTim)'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
     'sphinxcontrib.luadomain',
     'sphinx_lua'
 ]
@@ -42,6 +43,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
+# The theme to use for HTML and HTML Help pages. See the documentation for
+# a list of builtin themes.
+
+html theme = 'sphinx_rtd_theme'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
